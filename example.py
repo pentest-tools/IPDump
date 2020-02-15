77608dd36c28ae586d3e5ac5b0434a4d42e4b209
bruceybonus01 <adambruce0108@gmail.com>
2020-02-15 21:11:49 +0000

    Updated license

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2019 Adam Bruce
+# Copyright (c) 2020 Adam Bruce
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
